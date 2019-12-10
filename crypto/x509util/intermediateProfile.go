563cf6ea5ae943cb11d0dca2467d8609dc47f467
Wessel van der Veen <wvn@synadia.nl>
2019-12-10 14:11:08 +0100

    Add NewIntermediateProfileWithCSR

--- crypto/x509util/intermediateProfile.go
+++ crypto/x509util/intermediateProfile.go
@@ -5,6 +5,8 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 // DefaultIntermediateCertValidity is the default validity of a intermediate certificate in the step PKI.
@@ -22,11 +24,32 @@ func (i *Intermediate) DefaultDuration() time.Duration {
 
 // NewIntermediateProfile returns a new intermediate x509 Certificate profile.
 func NewIntermediateProfile(name string, iss *x509.Certificate, issPriv crypto.PrivateKey, withOps ...WithOption) (Profile, error) {
-	sub := defaultIntermediateTemplate(name)
+	sub := defaultIntermediateTemplate(pkix.Name{CommonName: name}, pkix.Name{CommonName: name})
 	return newProfile(&Intermediate{}, sub, iss, issPriv, withOps...)
 }
 
-func defaultIntermediateTemplate(name string) *x509.Certificate {
+// NewIntermediateProfileWithCSR returns a new intermediate x509 Certificate Profile with
+// Subject Certificate fields populated directly from the CSR.
+// A public/private keypair **WILL NOT** be generated for this profile because
+// the public key will be populated from the CSR.
+func NewIntermediateProfileWithCSR(csr *x509.CertificateRequest, iss *x509.Certificate, issPriv crypto.PrivateKey, withOps ...WithOption) (Profile, error) {
+	if csr.PublicKey == nil {
+		return nil, errors.Errorf("CSR must have PublicKey")
+	}
+
+	sub := defaultIntermediateTemplate(csr.Subject, iss.Subject)
+	sub.Extensions = csr.Extensions
+	sub.ExtraExtensions = csr.ExtraExtensions
+	sub.DNSNames = csr.DNSNames
+	sub.EmailAddresses = csr.EmailAddresses
+	sub.IPAddresses = csr.IPAddresses
+	sub.URIs = csr.URIs
+
+	withOps = append(withOps, WithPublicKey(csr.PublicKey))
+	return newProfile(&Intermediate{}, sub, iss, issPriv, withOps...)
+}
+
+func defaultIntermediateTemplate(sub pkix.Name, iss pkix.Name) *x509.Certificate {
 	notBefore := time.Now()
 	return &x509.Certificate{
 		IsCA:                  true,
@@ -36,7 +59,7 @@ func defaultIntermediateTemplate(name string) *x509.Certificate {
 		BasicConstraintsValid: true,
 		MaxPathLen:            0,
 		MaxPathLenZero:        true,
-		Issuer:                pkix.Name{CommonName: name},
-		Subject:               pkix.Name{CommonName: name},
+		Issuer:                iss,
+		Subject:               sub,
 	}
 }
